f68c6d76657fde04acc2970c478d44bbe13f8d6a
syedsaboorhashmi <syedsaboorhashmi@gmail.com>
2024-05-14 13:33:54 +0530

    navbar_2

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -31,7 +31,7 @@ const navData = [
 function Navbar() {
   return (
     <div className=" flex  bg-blue-900 w-full h-9  sm:py-0 py-2 ">
-      <div className="text-2xl text-white absolute right-8 top-24 cursor-pointer md:hidden">
+      <div className="text-2xl text-white absolute right-6 top-16 cursor-pointer md:hidden">
       <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M64 384h384v-42.67H64zm0-106.67h384v-42.66H64zM64 128v42.67h384V128z"></path></svg>
       </div>
 
