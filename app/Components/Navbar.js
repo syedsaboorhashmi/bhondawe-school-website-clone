f8144f24c1390c1cdbb8abdee2e7f3264e7a0af1
syedsaboorhashmi <syedsaboorhashmi@gmail.com>
2024-05-16 13:50:54 +0530

    navbar_final

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -1,45 +1,53 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
 
 
 const navData = [
   {
     id: 0,
     title: "Home",
-    path:"/"
+    path: "/",
   },
   {
     id: 1,
     title: "About Us",
-    path:"/about"
+    path: "/about",
   },
   {
     id: 2,
     title: "Admissions",
-    path:"/admissions"
+    path: "/admissions",
   },
-  { id: 3, title: "Mandatory Public Disclosure", path:"/mandatoryPublicDisclosure" },
-  { id: 4, title: "Academics",
-    path:"/academics"
+  {
+    id: 3,
+    title: "Mandatory Public Disclosure",
+    path: "/mandatoryPublicDisclosure",
   },
-  { id: 5, title: "Achievements", path:"/achievements" },
-  { id: 6, title: "Infrastructure",
-    path:"/infrastructure"
-  },
-  { id: 7, title: "Gallery", path:"/gallery" },
-  { id: 8, title: "Contact Us",path:"/contactUs" },
+  { id: 4, title: "Academics", path: "/academics" },
+  { id: 5, title: "Achievements", path: "/achievements" },
+  { id: 6, title: "Infrastructure", path: "/infrastructure" },
+  { id: 7, title: "Gallery", path: "/gallery" },
+  { id: 8, title: "Contact Us", path: "/contactUs" },
 ];
 function Navbar() {
+  const [open, setOpen]=useState(false);
   return (
     <div className=" flex  bg-blue-900 w-full h-9  sm:py-0 py-2 ">
-      <div className="text-2xl text-white absolute right-6 top-16 cursor-pointer md:hidden">
-      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M64 384h384v-42.67H64zm0-106.67h384v-42.66H64zM64 128v42.67h384V128z"></path></svg>
+      <div onClick={()=>setOpen(!open)} className="text-2xl text-white ml-auto cursor-pointer md:hidden">
+     {open?<IoMdClose />:  <FaBars /> }
+       
       </div>
 
-      <ul className="  pl-2 py-2 lg:flex md:flex sm:flex lg:gap-6 md:gap-3 sm:gap-2 text-white text-sm font-semibold">
-        {navData.map((item, i) => (<Link key={i} href={item.path}>
-          <li  className="hidden md:block lg:block xl:block">
-            {item.title}</li></Link>  
-          
+      <ul className=  {`pl-2 py-2  sm:static w-full z-50 bg-blue-900 transition delay-150 duration-500 ease-in-out lg:gap-6 md:gap-3 sm:gap-2 text-white text-sm font-semibold 
+      ${open? "absolute top-20 sm:flex ":"absolute -top-[28rem] sm:flex"}
+      `}>
+        {navData.map((item, i) => (
+          <Link key={i} href={item.path}>
+            <li className=" py-3 font-bold pl-2 border-b-2 rounded-sm sm:border-0 sm:flex sm:py-0">{item.title}</li>
+          </Link>
         ))}
       </ul>
     </div>
